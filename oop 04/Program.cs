23d78c885f2501cea37c5464f1291a18b0029e4a
ebrahimmohsen <ebrahimzaki456@gmail.com>
2025-07-19 20:15:25 +0300

    Q1-part1 solved

--- oop 04/Program.cs
+++ oop 04/Program.cs
@@ -1,9 +1,30 @@
 ﻿namespace oop_04;
 
+#region part 1  Q1 Write a class named Calculator that contains a method named Add. Overload the Add method
+class Calculator
+{
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int Add(int a, int b, int c)
+    {
+        return a + b + c;
+    }
+
+    public double Add(double a, double b)
+    {
+        return a + b;
+    }
+}
+
+#endregion
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+       
     }
 }
